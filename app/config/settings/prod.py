06820a32f171f2debb750d093b853095a932c684
maybottt <mayraromeliabotellomayta343@gmail.com>
2026-03-19 17:13:36 -0400

    configuracion inicial geticket

---
+++ app/config/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
